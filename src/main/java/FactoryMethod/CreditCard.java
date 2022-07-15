758be0616fc84d77c16d5a27decba610477bf036
Ramón Macías <rimacias@espol.edu.ec>
2022-07-14 21:38:20 -0500

    Initial commit

---
+++ src/main/java/FactoryMethod/CreditCard.java
@@ -0,0 +1,7 @@
+
+package FactoryMethod;
+
+public interface CreditCard {
+    
+    public boolean makePayment(double amount);
+}
---
+++ src/main/java/FactoryMethod/PremiumCC.java
@@ -0,0 +1,10 @@
+package FactoryMethod;
+
+public class PremiumCC implements CreditCard {
+
+    @Override
+    public boolean makePayment(double amount) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
