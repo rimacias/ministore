e8cd393f51f4a532c658e6f6c72c131051d744c0
Ramón Macías <rimacias@espol.edu.ec>
2022-07-15 20:28:48 -0500

    Correciones

--- src/main/java/Facade/Clients.java
+++ src/main/java/Facade/Clients.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Facade;
-
-/**
- *
- * @author CAELOS JR 2018
- */
 public class Clients {
-    
+    //TODO implement (Out of scope for this exercise)
 }
--- src/main/java/Facade/Movil.java
+++ src/main/java/Facade/Movil.java
@@ -1,29 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Facade;
-
-/**
- *
- * @author CAELOS JR 2018
- */
 public class Movil {
     private Integer IMEI;
     private Clients cliente;
-    
-    
     public void buy(){
         //se pide un producto al usuario
         Product producto = new Product("papa");
         ShopFacade comprador = new ShopFacade();
         comprador.ProcessPurchase(producto, this.cliente);
     }
-        
     public void Consult(){
         ShopFacade comprador = new ShopFacade();
         comprador.ConsultFactures(cliente);
     }
-
-    
 }
